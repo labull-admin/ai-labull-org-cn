ff4bdd66fd76cdd9116ce7b4f22e052fc7678f40
Softpug <104479318+Softpug@users.noreply.github.com>
2023-12-16 10:42:31 +0800

    build for windows

--- ai_labull_org_cn/lib/.env/env.dart
+++ ai_labull_org_cn/lib/.env/env.dart
@@ -2,4 +2,4 @@ const String DEV_STAGE = "development";
 const String STAGE_STAGE = "staging";
 const String PROD_STAGE = "production";
 
-String CURRENT_STAGE = DEV_STAGE;
+String CURRENT_STAGE = STAGE_STAGE;
